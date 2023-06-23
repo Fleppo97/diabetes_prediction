860a6df31ae409b265cb695fd943d88dffef8a40
Fleppo97 <69681340+Fleppo97@users.noreply.github.com>
2023-06-24 00:28:40 +0200

    Add files via upload

---
+++ installSparkOlderVersion.sh
@@ -0,0 +1,5 @@
+# INSTALLING SPARK 3.2.4
+SPARK_PATH="/home/bigdata2022"
+wget https://downloads.apache.org/spark/spark-3.2.4/spark-3.2.4-bin-hadoop3.2.tgz
+tar xzf spark-3.2.4-bin-hadoop3.2.tgz
+mv spark-3.2.4 "$SPARK_PATH"
